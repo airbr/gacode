e4b2185589336300c7c6b4de68ac57ec7e94a1e3
eareese <eareese@eareese.com>
2017-05-27 08:24:30 -0400

    Create a basic hardcoded tree-looking mithril component

--- src/app.js
+++ src/app.js
@@ -1,2 +1,27 @@
-var m = require('mithril')
-m.render(document.body, "hello world")
+import m from 'mithril'
+
+
+
+let Tree = {
+  view: () => {
+    return m('ul', [
+      m('li', 'Title One'),
+      m('ul', [
+        m('li', 'Chapter One'),
+        m('li', 'Chapter Two'),
+      ]),
+      m('li', 'Title Two'),
+      m('ul', [
+        m('li', 'Chapter One'),
+        m('li', 'Chapter Two'),
+      ]),
+      m('li', 'Title Three'),
+      m('ul', [
+        m('li', 'Chapter One'),
+        m('li', 'Chapter Two'),
+      ]),
+    ])
+  }
+}
+
+m.mount(document.body, Tree)
