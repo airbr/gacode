86aa97206d6b2b1a8c1f59fe1499f3505256a83c
eareese <eareese@eareese.com>
2017-05-27 09:54:21 -0400

    Create some fake data for development purposes

---
+++ src/development_data.js
@@ -0,0 +1,37 @@
+export const FAKE_TREE_DATA = {
+  titles: [
+    {
+      name: 'GENERAL PROVISIONS',
+      chapters: [
+        {
+          name: 'GENERAL PROVISIONS',
+          sections: [
+  	        { name: 'Enactment of Code' },
+  	        { name: 'Legislative intent' },
+  	        { name: 'Severability' },
+  	        { name: 'Validating Acts' },
+          ]
+        },
+      ]
+    },
+    {
+      name: 'AGRICULTURE',
+      chapters: [
+        {
+          name: 'Chapter 01',
+          sections: []
+        }
+      ]
+    },
+    {
+      name: 'ALCOHOL',
+      chapters: [
+        {
+          name: 'Chapter 01',
+          sections: []
+        }
+      ]
+    },
+  ]
+}
+
